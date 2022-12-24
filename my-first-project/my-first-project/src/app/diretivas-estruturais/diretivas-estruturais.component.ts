7eba1fdcf2f457b15ef91f26877084ef9c9b639b
Cascales-00 <58515219+Cascales-00@users.noreply.github.com>
2022-12-24 12:20:45 -0300

    ngSwitch

--- my-first-project/my-first-project/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ my-first-project/my-first-project/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./diretivas-estruturais.component.scss']
 })
 export class DiretivasEstruturaisComponent {
+  public condition: boolean = true;
+  public conditionClick: boolean =true;
+  public list: Array<{nome: string, idade: number}> = [
+    {nome: "Marjorie Gaioso", idade:23},
+    {nome: "Patricia", idade: 34},
+    {nome: "Elisangela", idade: 44} 
+  ];
 
+  public nome: string = 'Marjo';
+  constructor() {}
+
+  ngOnInit(): void{
+
+    setInterval(()=>{
+      if (this.condition){
+        this.condition = false;
+      }else{
+        this.condition = true;
+      } 
+    }, 2000)
+  }
+
+  public onClick(){
+    if (this.conditionClick){
+      this.conditionClick = false;
+    }else{
+      this.conditionClick = true;
+    } 
+  }
+
+  public onClickAddList(){
+    this.list.push({nome: "Nay", idade: 31});
+  }
+
+  public onClickEventList(event: number){
+    this.list.splice(event, 1)
+  }
 }
